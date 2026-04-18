100161c0c788eb4d6fd24cd53445d62622b7e874
Ditjon <dafaditjon@gmail.com>
2026-04-18 09:59:51 +0200

    Created a Sanity project into Think Round project.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Next App",
@@ -25,9 +25,10 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      {/* <body className="min-h-full flex flex-col">{children}</body> */}
+      <body>{children}</body>
     </html>
   );
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,18 +2,19 @@ import { User } from "../lib/definitions";
 import Header from "./components/header";
 import UserCard from "./components/user-card";
 import styles from "@/app/ui/users/users.module.css";
-export default async function Page() {
-  const data = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await data.json();
+export default function Page() {
+  // export default async function Page() {
+  // const data = await fetch("https://jsonplaceholder.typicode.com/users");
+  // const users = await data.json();
 
   return (
     <>
-      <Header />
+      {/* <Header />
       <div id={styles.users_list}>
         {users.map((user: User) => {
           return <UserCard key={user.id} {...user} />;
         })}
-      </div>
+      </div> */}
     </>
   );
 }
